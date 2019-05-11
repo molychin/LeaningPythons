f9b8e0604b06689bb592345f335f296d6cfba1fb
molychin <molychin@qq.com>
2019-05-11 23:14:13 +0800

    2019.05.11.23:58 molychin

---
+++ Python-100-Days/Day01-15/Day01/code/test02.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  9 10:01:27 2019
+
+@author: Administrator
+绘制图形练习
+"""
+
+#from turtle import *
+import turtle as tt
+
+tt.pensize(4)
+tt.pencolor('red')
+tt.forward(100)
+tt.right(90)
+tt.forward(100)
+tt.right(90)
+tt.forward(100)
+tt.right(90)
+tt.forward(100)
+tt.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
